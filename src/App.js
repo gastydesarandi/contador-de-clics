bc84a893425ecae3205ec5c1b179e173d9e6493a
Gaston Bundio <gasty_desarandi@hotmail.com>
2023-04-29 09:45:09 -0300

    cambios

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import './App.css';
 import Boton from './componentes/Boton';
 import Contador from './componentes/Contador';
 import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
+import{useState} from 'react';
 
 
 function App() {
+
+  const[numClics,setNumClics] = useState(0);
+
+
   const manejarClic=() => {
-    console.log ('Clic');
+    setNumClics (numClics + 1);
   }
-  const reiniciarContador= () => {
-    console.log ('Reiniciar');
+  const reiniciarContador=() => {
+    setNumClics (0);
   }
   return (
     <div className='App'>
@@ -20,7 +25,7 @@ function App() {
         alt='Logo de freeCodeCamp' />
       </div>
       <div className='contenedor-principal'>
-        <Contador numClics='5' />
+        <Contador numClics={numClics} />
         <Boton 
         texto='Clic'
         esBotonDeClic={true}
